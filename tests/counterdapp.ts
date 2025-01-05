dfda8ff4d420ba9eb5a690a1e53a4678faf5f73f
ayoubbijarche <ayoubbijarche6@gmail.com>
2025-01-05 19:48:07 +0100

    setup instructions to the ts test file

--- tests/counterdapp.ts
+++ tests/counterdapp.ts
@@ -2,27 +2,42 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Counterdapp } from "../target/types/counterdapp";
 
-anchor.setProvider(anchor.AnchorProvider.env());
-const program = anchor.workspace.Counterdapp as Program<Counterdapp>;
+
+
 describe("counterdapp", () => {
-
+  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  const program = anchor.workspace.Counterdapp as Program<Counterdapp>;
+  const counterAccount = anchor.web3.Keypair.generate();
+  
   it("Is initialized!", async () => {
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
-
-describe("increment value", () => {
+    const tx = await program.rpc.initialize({
+      accounts : {
+        countAcc : counterAccount.publicKey,
+        user : provider.wallet.publicKey,
+        systemProgram : anchor.web3.SystemProgram.programId,
+      },
+      signers : [counterAccount]
+    }as any);
+  })
+  
   it("increments value of the counter", async () => {
-    const increment_tx = await program.methods.increment().rpc();
+    const increment_tx = await program.rpc.increment({
+      accounts : {
+        countAcc : counterAccount
+      }
+    } as any);
     console.log("Your transaction signature", increment_tx);
   })
-})
-
-
-describe("decrement value", () => {
+  
   it("decrements value of the counter", async () => {
-    const decrement_tx = await program.methods.increment().rpc();
+    const decrement_tx = await program.rpc.decrement({
+      accounts : {
+        countAcc : counterAccount
+      }
+    } as any);
     console.log("Your transaction signature", decrement_tx);
   })
-})
+
+});
+
